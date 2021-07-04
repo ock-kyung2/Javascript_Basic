dc588e7fb3db60dacaf3d8fb9858a7d369e88d04
ock-kyung2 <dhrwlsrud94@naver.com>
2021-07-04 16:05:00 +0900

    type 종류, type of, "값이 없는"과 "선언되지않는 변수"의 차이

--- js/type.js
+++ js/type.js
@@ -1,1 +1,48 @@
-console.log(typeof undefined);
+console.log(typeof undefined); //undefined
+console.log(typeof true); //true
+console.log(typeof 42); //number
+console.log(typeof "42");  //string
+console.log(typeof {life: 42}); //object
+console.log(typeof Symbol()); // symbol
+console.log(typeof null); // object
+
+var a = null;
+console.log(!a && typeof a ); // object
+console.log(a && typeof a ); // null
+
+
+console.log(typeof function a(){/* */}); // function
+
+function foo(b,c){
+  /* 호출가능한 객체라서 prop 둘 수 있다.  */
+};
+console.log(foo.length); // 2
+
+console.log(typeof [1,2,3]); // object
+
+var b = 42;
+console.log(typeof b) // number
+
+c = true;
+console.log(typeof c) // boolean
+
+console.log(typeof typeof 42) // string
+
+// undefined vs undeclared
+
+var d;
+console.log(typeof d); // undefined
+
+var e = 42;
+var f;
+
+e=f;
+console.log(typeof e); // undefined
+console.log(typeof f); // undefined
+
+var g;
+g; // undefined
+// h; //ReferenceError: h is not defined != undefined
+
+console.log(typeof g); // undefined
+console.log(typeof h); // undefined
